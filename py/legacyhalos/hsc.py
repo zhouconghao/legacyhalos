e4b3d00600c77beb58c08359b5888ccf47995cf7
Conghao Zhou <zchusre@gmail.com>
2025-03-09 13:17:14 -0700

    avoid iterating empty galaxy index

--- py/legacyhalos/hsc.py
+++ py/legacyhalos/hsc.py
@@ -387,7 +387,7 @@ def _build_multiband_mask(data, tractor, filt2pixscale, fill_value=0.0,
 
     # If the row-index of the central galaxy is not provided, use the source
     # nearest to the center of the field.
-    if 'galaxy_indx' in data.keys():
+    if 'galaxy_indx' in data.keys() and len(data['galaxy_indx']) > 0:
         galaxy_indx = np.atleast_1d(data['galaxy_indx'])
     else:
         galaxy_indx = np.array([np.argmin((tractor.bx - data['refband_height']/2)**2 +
